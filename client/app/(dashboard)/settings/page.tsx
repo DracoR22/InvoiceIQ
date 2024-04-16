6f41d97459377fc3a28fcd99273f9bf89febf9ba
DracoR22 <rdraco039@gmail.com>
2024-04-16 14:19:03 -0500

    moving to prod

--- client/app/(dashboard)/settings/page.tsx
+++ client/app/(dashboard)/settings/page.tsx
@@ -1,12 +1,30 @@
+import { DeleteSection } from "@/components/delete-section";
+import { PreferencesForm } from "@/components/preferences-form";
 import TopMainContent from "@/components/top-main-content";
+import { getExtractions, getUserPreferences } from "@/lib/requests";
+import { Status } from "@prisma/client";
 
-export default function SettingsPage() {
-    return (
-        <>
-        <TopMainContent title="Settings" displayUploadButton/>
-        <div className="m-4">
-          <h1>Settings page</h1>
+export default async function SettingsPage() {
+  const preferences = await getUserPreferences();
+  const extractions = await getExtractions(Status.PROCESSED);
+  return (
+    <div className="h-full">
+      <TopMainContent title="Settings" displayUploadButton />
+      <div className="m-8 flex flex-col flex-grow space-y-10 2xl:space-y-6">
+        <div>
+          <h1 className="text-2xl font-bold text-slate-900 mb-2">
+            Data Extraction Preferences
+          </h1>
+          <PreferencesForm
+            preferences={preferences}
+            extractions={extractions}
+          />
         </div>
-        </>
-    )
+        <div>
+          <h1 className="text-2xl font-bold text-slate-900 mb-2">Account</h1>
+          <DeleteSection/>
+        </div>
+      </div>
+    </div>
+  );
 }
